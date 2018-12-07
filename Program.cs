8f87a373e2e4ee198d6cc12adc807fee2b6e0ace
Magicrafter13 <scubaventure101@hotmail.com>
2018-12-06 19:12:06 -0800

    Initial Code Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HouseCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
